2d036ee585d6f494cee5039596c9e330e7ef2949
David <david.richter@freenet.de>
2020-07-26 14:36:43 +0200

    A commit that includes the entire project, because I did it a few years back but never put it up on github, therefore it will not be pushed in smaller commits. Not very pretty but it works I guess...

---
+++ Test Project/Assets/MyAssets/BlueMoon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueMoon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.up * 42 * Time.deltaTime);
+        transform.Rotate(Vector3.back * Time.deltaTime * 6);
+
+	}
+}
---
+++ Test Project/Assets/MyAssets/Collect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Collect : MonoBehaviour {
+
+    public int remaining;
+    public Text remainingText;
+    public Text winningText;
+
+	// Use this for initialization
+	void Start () {
+        remaining = 3;
+        SetRemainingText();
+        winningText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coins)
+    {
+        if (coins.gameObject.CompareTag("Coin"))
+            Destroy(coins.gameObject);
+        //coins.gameObject.SetActive(false);
+        remaining--;
+        SetRemainingText();
+    }
+
+    void SetRemainingText()
+    {
+        remainingText.text = "Coins Remaining: " + remaining.ToString();
+        if (remaining <= 0)
+            winningText.text = "The Cube has launched!";
+    }
+
+}
